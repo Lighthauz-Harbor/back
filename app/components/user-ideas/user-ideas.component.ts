7cda75b1a552cfaa9a9cbe1310ab9b3648a99483
Baskoro Indrayana <baskoroindrayana@outlook.com>
2016-12-24 12:53:45 +0700

    Implemented populated list of user's ideas component

--- app/components/user-ideas/user-ideas.component.ts
+++ app/components/user-ideas/user-ideas.component.ts
@@ -12,6 +12,9 @@ import { IdeasService } from "../../services/ideas.service";
 })
 export class UserIdeasComponent implements OnInit {
 
+    private ideas: Idea[] = [];
+    private message: string = "Loading...";
+
     constructor(
         private router: Router,
         private route: ActivatedRoute,
@@ -20,7 +23,36 @@ export class UserIdeasComponent implements OnInit {
     }
 
     ngOnInit(): void {
+        this.route.params.forEach((params: Params) => {
+            let id = params["id"];
+            this.ideasService.getIdeaListFromUser(id)
+                .subscribe((json: any) => {
+                    if (json.fail) {
+                        this.message = json.fail;
+                    } else {
+                        this.pushIdeas(json);
+                        if (this.ideas.length === 0) {
+                            this.message = "The user has not created any ideas.";
+                        } else {
+                            this.message = "";
+                        }
+                    }
+                });
+        });
+    }
 
+    private pushIdeas(json: any): void {
+        json.ideas.map((item: any) => {
+            this.ideas.push(
+                new Idea(
+                    item.id, 
+                    {
+                        title: item.title,
+                        description: item.description,
+                        picture: item.pic,
+                    }, {}, {},
+                    "", item.category, new Date(item.timestamp)));
+            });
     }
 
 }
